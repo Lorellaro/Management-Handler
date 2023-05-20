20d76ee8f66d44881f920cdbc2ae7bf35714ef24
Lorellaro <lorenzoscudellaro@gmail.com>
2023-05-20 14:30:16 +0100

    Added Database Tables and migrations. End implement database section

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+    }
+}
