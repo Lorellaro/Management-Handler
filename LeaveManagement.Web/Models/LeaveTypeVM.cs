21cde03c5e1d8e766cf2f9fc37bc31ccacd46425
Lorellaro <lorenzoscudellaro@gmail.com>
2023-05-22 18:41:45 +0100

    Modified crud scenes and  added datatables and swal.

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter A Valid Number Between 1 And 25")]
+        public int DefaultDays { get; set; }
+    }
+}
